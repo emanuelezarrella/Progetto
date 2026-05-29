ec931369258456ba89083f616fdff4ec3daed05e
Carmine <c.renna2@studenti.unisa.it>
2026-05-29 19:10:15 +0200

    Delete attesa.h
    gia implementata precedentemente

--- attesa.h
+++
@@ -1,8 +0,0 @@
-#include "item.h"
-#include "list.h"
-
-void aggiungi_attesa(list *attesa, item matricola);
-
-void rimuovi_attesa(list *attesa, item matricola);
-
-void visualizza_attesa(list attesa);
