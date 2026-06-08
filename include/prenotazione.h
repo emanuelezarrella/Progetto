652fb301da576f6d3b4a1c9ce037e24051df5e22
Emanuele <emanuele.zarrella16@gmail.com>
2026-06-09 00:42:25 +0200

    feat: riscritto il file prenotazione.c _______________________________________
    In questa versione ho riscritto completamente il file prenotazione.c, apportando le seguenti modifiche:
    - implementato una Typedef per la prenotazione, come fatto con studente, che contiene 3 campi ovvero matricola, data e orario
    - implementato le funzioni di crea posto, registra prenotazione e cerca_posto, che sostituiscono le funzioni precedenti di prenotazione

--- include/prenotazione.h
+++ include/prenotazione.h
@@ -10,10 +10,16 @@ extern list lista_primopomeriggio;
 extern list lista_secondopomeriggio;
 //come in studente.h
 
-void verifica_disponibilità_data(int data);
-void inserimento_prenotazione(Studente *s, const char *matricola);
-Studente *trova_posto(Studente *s, const char *matricola);
-void verifica_disponibilità_orario(int orario, list lista_mattina, list lista_primopomeriggio, list lista_secondopomeriggio);
-void checkin_studente(Studente *s, list lista_mattina, list lista_primopomeriggio, list lista_secondopomeriggio);
+typedef struct prenotazione{
+    char matricola[MAX_MATRICOLA];
+    int data;
+    int orario; //1 per mattina, 2 per primo pomeriggio, 3 per secondo pomeriggio
+} Prenotazione;
 
-#endif /* PRENOTAZIONE_H */
+Prenotazione *crea_posto(Studente *s, Prenotazione *p, const char *matricola);
+void registra_prenotazione(Studente *s, Prenotazione *p);
+void cerca_posto(Studente *s, Prenotazione *p, const char *matricola);
+void verifica_disponibilità_orario(int orario, list lista_mattina, list lista_primopomeriggio, list lista_secondopomeriggio);
+//void checkin_studente(Studente *s, list lista_mattina, list lista_primopomeriggio, list lista_secondopomeriggio);
+
+#endif 
